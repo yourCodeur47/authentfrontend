fef252278259c983f2ac7bb47ba57b6822f3caf3
codeur47 <yoropapers@gmail.com>
2025-04-29 00:41:07 +0200

    feat: update angular v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true
 })
 export class AppComponent {
   title = 'autentfrontend';
